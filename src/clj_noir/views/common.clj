2b26155ca278b1c95ee1d21ef253c8b26c4d2b5c
Chris Granger <ibdknox@gmail.com>
2011-06-21 17:11:02 -0700

    add analytics
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/clj_noir/views/common.clj
+++ src/clj_noir/views/common.clj
@@ -3,6 +3,21 @@
        hiccup.core
        hiccup.page-helpers))
 
+(def analytics "
+<script type=\"text/javascript\">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24109853-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>")
+
 (defpartial footer []
             [:div#footer
              [:p "Copyright © 2011 " (link-to "http://chris-granger.com" "Chris Granger") ". All rights reserved."]])
@@ -13,7 +28,8 @@
                [:title "Noir"]
                (include-css "/css/reset.css")
                (include-css "/css/noir.css")
-               (include-css "/css/gist.css")]
+               (include-css "/css/gist.css")
+               analytics]
               [:body
                [:div#wrapper
                 [:div#content
