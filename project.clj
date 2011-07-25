c9f36f0a956b66359a69ddbed00fdacc61647822
Chris Granger <ibdknox@gmail.com>
2011-07-24 23:55:42 -0700

    prepare for 1.1.0
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webnoir "0.1.0-SNAPSHOT"
+(defproject webnoir "0.1.0"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [noir "1.1.0"]
