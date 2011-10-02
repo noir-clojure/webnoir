3ab2b0d689a4733ead6bb8ac68229d6ac67ef9ff
Chris Granger <ibdknox@gmail.com>
2011-10-02 13:25:28 -0700

    add 1.2.0 :D
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject webnoir "0.1.0"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.1.0"]
+                           [noir "1.2.0"]
                            [clj-markdown "0.1.0"]]
             :main webnoir.server)
 
